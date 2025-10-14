0c37edbab073a2ed721283a6d987df62567f6e26
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-10-13 22:28:12 -0400

    feat: add Consumable class that extends Item

---
+++ src/main/java/com/bptn/rpg/model/Consumable.java
@@ -0,0 +1,19 @@
+package com.bptn.rpg.model;
+
+public class Consumable extends Item {
+	private int potency;
+
+	public Consumable(String name, Rarity rarity, int value, int potency) {
+		super(name, ItemType.CONSUMABLE, rarity, value);
+		this.potency = potency;
+	}
+
+	public int getPotency() {
+		return potency;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " | Potency" + potency;
+	}
+}
