2b76e2a27f95f66dee04874296410cc6257c05ae
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-10-13 21:55:26 -0400

    feat: add Item class model

--- src/main/java/com/bptn/rpg/App.java
+++ src/main/java/com/bptn/rpg/App.java
@@ -4,10 +4,8 @@ package com.bptn.rpg;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
---
+++ src/main/java/com/bptn/rpg/model/Item.java
@@ -0,0 +1,37 @@
+package com.bptn.rpg.model;
+
+public class Item {
+	private String name;
+	private ItemType type;
+	private Rarity rarity;
+	private int value;
+
+	public Item(String name, ItemType type, Rarity rarity, int value) {
+		this.name = name;
+		this.type = type;
+		this.rarity = rarity;
+		this.value = value;
+	}
+
+	// Getters & Setters
+	public String getName() {
+		return name;
+	}
+
+	public ItemType getItemType() {
+		return type;
+	}
+
+	public Rarity getRarity() {
+		return rarity;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	@Override
+	public String toString() {
+		return name + " (" + rarity + ")";
+	}
+}
