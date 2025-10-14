4e429996fa72b9269fa657f976f02734cabc5bf6
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-10-13 21:41:02 -0400

    feat: add Rarity enum modle

---
+++ src/main/java/com/bptn/rpg/model/Rarity.java
@@ -0,0 +1,16 @@
+package com.bptn.rpg.model;
+
+public enum Rarity {
+	COMMON("Common"), UNCOMMON("Uncommon"), RARE("Rare"), EPIC("Epic"), LEGENDARY("Legendary");
+
+	private final String displayName;
+
+	private Rarity(String displayName) {
+		this.displayName = displayName;
+	}
+
+	@Override
+	public String toString() {
+		return displayName;
+	}
+}
