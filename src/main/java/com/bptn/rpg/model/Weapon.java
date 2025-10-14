2a3694fdf7813e309fa1e5222aed5f6f3168536c
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-10-13 22:19:06 -0400

    feat: add Weapon class which extends Item

---
+++ src/main/java/com/bptn/rpg/model/Weapon.java
@@ -0,0 +1,19 @@
+package com.bptn.rpg.model;
+
+public class Weapon extends Item {
+	private int strength;
+
+	public Weapon(String name, Rarity rarity, int value, int strength) {
+		super(name, ItemType.WEAPON, rarity, value);
+		this.strength = strength;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " | Strength: " + strength;
+	}
+}
