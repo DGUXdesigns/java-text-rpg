6f9d8b133f828e33be2e7bbaa8733d4bd97fc14b
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-10-16 01:34:55 -0400

    feat: Implement basic game loop and hero creation

--- src/main/java/com/bptn/rpg/App.java
+++ src/main/java/com/bptn/rpg/App.java
@@ -1,11 +1,48 @@
 package com.bptn.rpg;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+import com.bptn.rpg.model.character.Hero;
+import com.bptn.rpg.service.ShopService;
+
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		// Get Hero Name
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("===========================");
+		System.out.println("⚔️ What is your name Hero?");
+		System.out.println("===========================");
+
+		String heroName = scanner.next();
+
+		// Create Hero
+		Hero hero = new Hero(heroName, 100, 50);
+
+		boolean gameRunning = true;
+
+		while (gameRunning) {
+			System.out.println("Where to?");
+
+			String[] options = { "Shop", "Training Dungeon", "Slay The Dragon" };
+			int index = 1;
+
+			for (String option : options) {
+				System.out.println(index++ + ". " + option);
+			}
+
+			System.out.println("0. Exit the game");
+			int choice = scanner.nextInt();
+
+			if (choice == 0) {
+				gameRunning = false;
+				System.out.println("Thanks For Playing!");
+			} else if (choice == 1) {
+				ShopService shop = new ShopService(scanner);
+				shop.open(hero);
+			} else {
+				System.out.println("Feature in the works...");
+			}
+		}
+
 	}
 }
