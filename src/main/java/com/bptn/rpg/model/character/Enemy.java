bf83c0a296b83e46bc72bb5d2fcb41f5dba28656
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-10-18 13:40:17 -0400

    feat: Implement Enemy class combat mechanics
    - Add experience and gold reward for defeating enemies
    - Implement attack logic with random damage and level scaling
    - Add defensive stance functionality
    - Complete flee mechanic with 50% escape chance
    - Add new constructor parameters for level and rewards
    - Initialize Random instance for damage calculations

--- src/main/java/com/bptn/rpg/model/character/Enemy.java
+++ src/main/java/com/bptn/rpg/model/character/Enemy.java
@@ -2,33 +2,58 @@ package com.bptn.rpg.model.character;
 
 import com.bptn.rpg.model.item.Consumable;
 
+import java.util.Random;
+
 public class Enemy extends Character {
-	public Enemy(String name, int health, int strength, int level) {
-		super(name, health, strength);
-	}
+    private final int experienceReward;
+    private final int goldReward;
+    private final Random random = new Random();
 
-	@Override
-	public void attack(Character target) {
-		// TODO Auto-generated method stub
 
-	}
+    public Enemy(String name, int health, int strength, int level, int experienceReward, int goldReward) {
+        super(name, level, health, strength);
+        this.experienceReward = experienceReward;
+        this.goldReward = goldReward;
+    }
 
-	@Override
-	public void defend() {
-		// TODO Auto-generated method stub
+    public int getExperienceReward() {
+        return experienceReward;
+    }
 
-	}
+    public int getGoldReward() {
+        return goldReward;
+    }
 
-	@Override
-	public void useItem(Consumable item, Character target) {
-		// TODO Auto-generated method stub
+    @Override
+    public void attack(Character target) {
+        int damage = getStrength() + random.nextInt(5) + (getLevel() * 2);
+        System.out.println(getName() + " dealt " + damage + " damage!");
+        target.takeDamage(damage);
+    }
 
-	}
+    @Override
+    public void defend() {
+        setDefending(true);
+        System.out.println(getName() + "takes a defensive stance and will take reduced damage next turn");
+    }
 
-	@Override
-	public boolean flee() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public void useItem(Consumable item, Character target) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean flee() {
+        double fleeChance = Math.random();
+        // 50% chance to escape
+        if (fleeChance > 0.5) {
+            System.out.println(getName() + " successfully fled the battle!");
+            return true;
+        } else {
+            System.out.println(getName() + " tried to flee but couldn't get away!");
+            return false;
+        }
+    }
 
 }
