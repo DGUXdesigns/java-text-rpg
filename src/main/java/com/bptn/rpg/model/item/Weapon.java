b4b9a0805a76eb8d996c006ac68454cdac435c6d
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-10-19 17:54:16 -0400

    refactor: make damage field immutable

--- src/main/java/com/bptn/rpg/model/item/Weapon.java
+++ src/main/java/com/bptn/rpg/model/item/Weapon.java
@@ -1,19 +1,19 @@
 package com.bptn.rpg.model.item;
 
 public class Weapon extends Item {
-	private int damage;
+    private final int damage;
 
-	public Weapon(String name, Rarity rarity, int price, int damage) {
-		super(name, ItemType.WEAPON, rarity, price);
-		this.damage = damage;
-	}
+    public Weapon(String name, Rarity rarity, int price, int damage) {
+        super(name, ItemType.WEAPON, rarity, price);
+        this.damage = damage;
+    }
 
-	public int getDamage() {
-		return damage;
-	}
+    public int getDamage() {
+        return damage;
+    }
 
-	@Override
-	public String toString() {
-		return super.toString() + " | Damage: " + damage;
-	}
+    @Override
+    public String toString() {
+        return super.toString() + " | Damage: " + damage;
+    }
 }
