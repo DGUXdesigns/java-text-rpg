2bfdf143d5e2cdcbad6e273e6c5fb2221eec55dd
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-10-19 19:51:40 -0400

    refactor: Update attack return type

--- src/main/java/com/bptn/rpg/model/character/Commands.java
+++ src/main/java/com/bptn/rpg/model/character/Commands.java
@@ -3,11 +3,11 @@ package com.bptn.rpg.model.character;
 import com.bptn.rpg.model.item.Consumable;
 
 public interface Commands {
-	public void attack(Character target);
+    public int attack(Character target);
 
-	public void defend();
+    public void defend();
 
-	public void useItem(Consumable item, Character target);
+    public void useItem(Consumable item, Character target);
 
-	public boolean flee();
+    public boolean flee();
 }
