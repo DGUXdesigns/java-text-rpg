b17627a7039879019ef6ec2c6613ec30fc922ed4
Daveian Gordon <140139328+DGUXdesigns@users.noreply.github.com>
2025-10-19 21:42:56 -0400

    refactor: remove unused field

--- src/main/java/com/bptn/rpg/utils/Messages.java
+++ src/main/java/com/bptn/rpg/utils/Messages.java
@@ -11,7 +11,6 @@ public class Messages {
     public static final String BLUE = "\u001B[34m";
     public static final String PURPLE = "\u001B[35m";
     public static final String CYAN = "\u001B[36m";
-    public static final String WHITE = "\u001B[37m";
 
     public static void attack(Character attacker, Character target, int damage, boolean isDefending) {
         System.out.println(RED + "\uD83D\uDDE1️ " + attacker.getName() + " attacks " + target.getName() + " for " + damage + " damage!\n" + RESET);
